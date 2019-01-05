b1815a6e8fdd406c167cf0ad587af4e086c56597
phtrungit <32809538+phtrungit@users.noreply.github.com>
2019-01-05 12:45:42 +0700

    Update SignInComponent.js

--- src/component/SignInComponent.js
+++ src/component/SignInComponent.js
@@ -74,7 +74,7 @@ class SignIn extends React.Component{
         event.preventDefault()
         console.log('handleSubmit')
         axios
-            .post('http://localhost:4200/login', {
+            .post('https://server-dack-mhh.herokuapp.com/login', {
                 username:this.state.username,
                 password:this.state.password
             })
